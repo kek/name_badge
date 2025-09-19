f3f5beb28b52d633d0576e2aaf8a85506d6bfb20
Karl Eklund <localpart@gmail.com>
2025-09-19 16:49:34 +0200

    Remove schedule

--- lib/name_badge/screen/top_level.ex
+++ lib/name_badge/screen/top_level.ex
@@ -6,7 +6,7 @@ defmodule NameBadge.Screen.TopLevel do
   @screens [
     {"Name Badge", NameBadge.Screen.NameBadge},
     {"Gallery", NameBadge.Screen.Gallery},
-    {"Schedule", NameBadge.Screen.Schedule},
+    # {"Schedule", NameBadge.Screen.Schedule},
     {"Settings", NameBadge.Screen.Settings}
   ]
 
